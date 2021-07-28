91b260f534ee6f07c56c59f21a3b901d603a2864
Taopol BERQUER <tao.berquer@gmail.com>
2021-07-28 23:21:44 +0200

    Ajout de store

---
+++ app/Models/Store.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Relations\EmbedsMany;
+use Jenssegers\Mongodb\Relations\HasMany;
+
+class Store extends Model
+{
+    protected $primaryKey = 'store_id';
+
+    protected $guarded = [];
+
+    public function restaurantStatus(): EmbedsMany
+    {
+        return $this->embedsMany(RestaurantStatus::class, 'store_id');
+    }
+
+    public function holidayHours(): EmbedsMany
+    {
+        return $this->embedsMany(HolidayHour::class, 'store_id');
+    }
+
+    public function menus(): HasMany
+    {
+        return $this->hasMany(Menu::class, 'store_id');
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class, 'store_id');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class, 'store_id');
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'store_id');
+    }
+}
